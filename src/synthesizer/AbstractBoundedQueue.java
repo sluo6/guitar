b582690296a7fab9a0f60e1313beb16f0b55a389
Shangwen Luo <shangwenluo6@gmail.com>
2017-04-23 08:30:34 -0500

    Did some work on AbstractBoundedQueue

--- src/synthesizer/AbstractBoundedQueue.java
+++ src/synthesizer/AbstractBoundedQueue.java
@@ -1,5 +1,22 @@
 package synthesizer;
 
-public class AbstractBoundedQueue {
+public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T>{
+	protected int fillCount;
+	protected int capacity;
+	public int capacity() {
+		return 0;
+	}
+	public int fillCount() {
+		return 0;
+	}
+	public boolean isEmpty() {
+		return false;
+	}
+	public boolean isFull() {
+		return false;
+	}
+	public abstract T peek();
+	public abstract T dequeue();
+	public abstract void enqueue(T x);
 
 }

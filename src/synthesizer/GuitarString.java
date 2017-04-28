067c15343c4a2bf8e85eb46e75263bf3ac3f49a6
Shangwen Luo <shangwenluo6@gmail.com>
2017-04-27 22:45:31 -0500

    Started GuitarString class

--- src/synthesizer/GuitarString.java
+++ src/synthesizer/GuitarString.java
@@ -9,6 +9,8 @@ public class GuitarString {
 
     /* Buffer for storing sound data. */
     private BoundedQueue<Double> buffer;
+    
+
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
